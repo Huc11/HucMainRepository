c20c5a6a1ea27ea71f79403abc1732207600c915
Jianghai Hu <hujianghai3637@gmail.com>
2024-12-08 22:31:48 -0500

    update for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cd ServerFolder && npm start
--- ServerFolder/app.js
+++ ServerFolder/app.js
@@ -8,6 +8,7 @@ const userRoutes = require('./routes/user');
 const chatRoutes = require('./routes/chat');
 const codeRoutes = require('./routes/code'); // 引入 code 路由
 const nbaRoutes = require('./routes/nba');
+const path = require('path');
 
 const app = express();
 
@@ -18,10 +19,7 @@ connectToDatabase();
 app.use(cors());
 app.use(bodyParser.json());
 
-// 路由
-app.get('/', (req, res) => {
-  res.send("Server is up and running!");
-});
+
 
 app.use('/user', userRoutes); // 用户注册和登录路由
 app.use('/api', chatRoutes); // 聊天路由和获取模型列表
@@ -29,6 +27,11 @@ app.use('/api/code', codeRoutes);//代码仓库
 
 
 app.use('/api/nba', nbaRoutes);//nba Routes
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 // 启动服务器
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
